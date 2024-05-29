9b4a65c94a51faa33c3b9f918b4a6e1a21773b74
Stef24Dev <stefanmuntianu2@gmail.com>
2024-05-29 16:49:05 +0200

    modified frontend, finished home page

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ### Project work Muntianu Stefan
 
 Project work for Fitstic ITS in devops and cloud
+
+Analisi dei dati sulla distribuzione e cantieri sulla Fibra ed FWA in italia e nelle varie regioni
+
+Utilizzo delle tecnologie:
+    Frontend --> React
+    Backend --> Flask e Pandas
+    Container --> Docker
--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -2,47 +2,49 @@ import React from "react";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 import Container from 'react-bootstrap/Container';
+import Button from "react-bootstrap/esm/Button";
+import { Link } from 'react-router-dom';
 
-const stringa = `            TODO: SIstemare i titoli, l'andare a capo e tutto quanto <br />
-Introduzione
-Il progetto Banda Ultra Larga (BUL) rappresenta un fondamentale pilastro nell'evoluzione delle infrastrutture di connettività, mirando a fornire accesso ad Internet ad alta velocità a una vasta gamma di utenti. In questo contesto, l'obiettivo principale di questo progetto Jupyter è condurre un'analisi dettagliata dei dati relativi alla copertura, alla qualità del servizio e seguire i lavori di avanzamento per due tecnologie chiave: la Fibra Ottica (FTTH) e il Fixed Wireless Access (FWA).
 
-Obbiettivo
-Questa analisi si propone di fornire una panoramica dello stato attuale del progetto BUL, con particolare attenzione alle tecnologie di connettività e ai lavori dei cantieri. Speriamo che le conclusioni tratte da questa analisi possano essere utili.
-
-Introduzione del progetto
-In questa parte importiamo le librerie che ci serviranno durante il progetto e il DataFrame da un file '.csv'.
-
-Glossario per lo stato dei cantieri
-In progettazione
-
-Vuol dire che il piano non è ancora stato emesso o che è stato emesso ma il progetto del cantiere è ancora in progettazione.
-In esecuzione
-
-Vuol dire che il cantiere è in esecuzione.
-Terminato
-
-Vuol dire che il cantiere è chiuso e la fibra o è in collaudo per il test o è già stata collaudata, quindi funzionante.
-In funzione
-
-Vuol dire che la fibra è in funzione con lavori terminati e collaudati.`
+function Choice(choice:string) {
+    console.log(`Pulsante premuto sulla scelta: ${choice}`)
+}
 
 export default function Home() {
-    return <>
+    return <>            
         <Container className="home">
-            <Row>
-                <Col>Introduzione</Col>
-            </Row>
-            <Row>
-                <Col>
-                    TODO: SIstemare i titoli, l'andare a capo e tutto quanto
-                    Il progetto Banda Ultra Larga (BUL) rappresenta un fondamentale pilastro nell'evoluzione delle infrastrutture di connettività, mirando a fornire accesso ad Internet ad alta velocità a una vasta gamma di utenti. In questo contesto, l'obiettivo principale di questo progetto Jupyter è condurre un'analisi dettagliata dei dati relativi alla copertura, alla qualità del servizio e seguire i lavori di avanzamento per due tecnologie chiave: la Fibra Ottica (FTTH) e il Fixed Wireless Access (FWA).
-                </Col>
+            <Row className="choice">
+                Introduzione<br /><br />
+                Il progetto Banda Ultra Larga (BUL) rappresenta un fondamentale pilastro nell'evoluzione delle infrastrutture di connettività, mirando a fornire accesso ad Internet ad alta velocità a una vasta gamma di utenti.<br />
+                In questo contesto, l'obiettivo principale di questo progetto è condurre un'analisi dettagliata dei dati relativi alla copertura, alla qualità del servizio e seguire i lavori di avanzamento per due tecnologie chiave: la Fibra Ottica (FTTH) e il Fixed Wireless Access (FWA).<br /><br />
+
+                Obbiettivo<br /><br />
+                Questa analisi si propone di fornire una panoramica dello stato attuale del progetto BUL, con particolare attenzione alle tecnologie di connettività e ai lavori dei cantieri.<br /><br />
+
+                <div className="glossary">
+                    Glossario per lo stato dei cantieri<br /><br />
+
+                    In progettazione<br />
+                    Vuol dire che il piano non è ancora stato emesso o che è stato emesso ma il progetto del cantiere è ancora in progettazione.<br /><br />
+                    
+                    In esecuzione<br />
+                    Vuol dire che il cantiere è in esecuzione.<br /><br />
+                    
+                    Terminato<br />
+                    Vuol dire che il cantiere è chiuso e la fibra o è in collaudo per il test o è già stata collaudata, quindi funzionante.<br /><br />
+                    
+                    In funzione<br />
+                    Vuol dire che la fibra è in funzione con lavori terminati e collaudati.
+                </div>
             </Row>
         </Container>
         <Row>
-            <Col className="choice">Analisi sulla Fibra ed FWA a livello nazionale</Col>
-            <Col className="choice">Analisi sulla Fibra ed FWA a livello regionale</Col>
+            <Col>
+                <Link className="link" to='/nazionale'>Analisi sulla Fibra ed FWA a livello nazionale</Link>
+            </Col>
+            <Col>
+               <Link className='link' to='/regionale'>Analisi sulla Fibra ed FWA a livello regionale</Link>
+            </Col>
         </Row>
     </>
 }
