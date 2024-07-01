e7721785764b76d18b59ebbecda03fa1009e1724
Stef24Dev <stefanmuntianu2@gmail.com>
2024-07-01 08:54:27 +0200

    fixed design

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,10 +9,14 @@ export default function FooterStef() {
     return <>
         <Row className="bg_footer justify-content-center">
             <Col xs="auto" className="footer_content">
-                <Icon.Github size = {50}/>
+                <a href="https://github.com/Stef24Dev">
+                    <Icon.Github size = {50}/>
+                </a>
             </Col>
             <Col xs="auto" className="footer_content">
-                <Icon.Envelope size = {50}/>
+                <a href="mailto:stefanmuntianu2@gmail.com">
+                    <Icon.Envelope size = {50}/>
+                </a>
             </Col>
         </Row>
     </>
